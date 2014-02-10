1833f7494fb9aac3a64733a806d50d39fc3cdb41
joamag <joamag@gmail.com>
2014-02-10 13:01:27 +0000

    initial cli structure

--- src/migratore/__init__.py
+++ src/migratore/__init__.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import base
+import cli
 import info
 import loader
 import migration
 import mysql
 
 from base import *
+from cli import *
 from info import *
 from loader import *
 from migration import *
--- src/migratore/info.py
+++ src/migratore/info.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 NAME = "migratore"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 AUTHOR = "Hive Solutions Lda."
 EMAIL = "development@hive.pt"
 DESCRIPTION = "Migratore Infra-structure"
