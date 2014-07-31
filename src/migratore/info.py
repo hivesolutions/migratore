93bcda9249f01b5698e62dfc89cf90dbe539ef6f
joamag <joamag@gmail.com>
2014-07-31 18:34:39 +0100

    new version

--- src/migratore/info.py
+++ src/migratore/info.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 NAME = "migratore"
-VERSION = "0.2.2"
+VERSION = "0.2.3"
 AUTHOR = "Hive Solutions Lda."
 EMAIL = "development@hive.pt"
 DESCRIPTION = "Migratore Infra-structure"
 LICENSE = "GNU General Public License (GPL), Version 3"
 KEYWORDS = "migratore migration sql"
-URL = "http://migratore.com"
+URL = "http://migratore.hive.pt"
 COPYRIGHT = "2008-2014 Hive Solutions Lda."
