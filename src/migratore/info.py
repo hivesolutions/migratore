2207552cd2bfc68d9f2f52b1a2f1d19245ba6e07
joamag <joamag@gmail.com>
2014-08-04 12:28:24 +0100

    new version

--- src/migratore/info.py
+++ src/migratore/info.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 NAME = "migratore"
-VERSION = "0.2.7"
+VERSION = "0.2.8"
 AUTHOR = "Hive Solutions Lda."
 EMAIL = "development@hive.pt"
 DESCRIPTION = "Migratore Infra-structure"
