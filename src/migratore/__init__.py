be80388ce8c4a2a1fe3c9de1fb238224c45f463f
joamag <joamag@gmail.com>
2014-11-13 15:45:30 +0000

    better import structure

--- src/migratore/__init__.py
+++ src/migratore/__init__.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import base
-import cli
-import info
-import loader
-import migration
-import mysql
+from . import base
+from . import cli
+from . import info
+from . import loader
+from . import migration
+from . import mysql
 
-from base import *
-from cli import *
-from info import *
-from loader import *
-from migration import *
-from mysql import *
+from .base import Migratore, Console, Database, Table, Result
+from .cli import run_help, run_version, run_environ, run_list, run_errors, run_trace,\
+    run_rebuild, run_upgrade, run_generate, main
+from .info import NAME, VERSION, AUTHOR, EMAIL, DESCRIPTION, LICENSE, KEYWORDS, URL,\
+    COPYRIGHT
+from .loader import Loader, DirectoryLoader
+from .migration import Migration
+from .mysql import MySQLDatabase
