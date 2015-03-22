ce5cee751c1b26cab66866d0796465d97611cdcc
joamag <joamag@gmail.com>
2015-03-22 21:13:00 +0000

    new type

--- src/migratore/test/base.py
+++ src/migratore/test/base.py
@@ -26,5 +26,5 @@ class BaseTest(unittest.TestCase):
         self.assertEqual(db.exists_table("users_extra"), False)
         self.assertEqual(table.has_column("username"), True)
         self.assertEqual(table.has_column("password"), True)
-        self.assertEqual(table.type_column("username"), "varchar")
-        self.assertEqual(table.type_column("password"), "varchar")
+        self.assertEqual(table.type_column("username"), "longtext")
+        self.assertEqual(table.type_column("password"), "longtext")
