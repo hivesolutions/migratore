9532d3793e8e24b11b029f434359afe24df38b13
joamag <joamag@gmail.com>
2015-03-22 20:28:27 +0000

    reverted to old code (no skip)

--- src/migratore/test/base.py
+++ src/migratore/test/base.py
@@ -17,15 +17,7 @@ class BaseTest(unittest.TestCase):
         self.assertEqual(type(result), legacy.UNICODE)
 
     def test_create(self):
-        db = self._get_db()
-        if not db: return
+        db = migratore.Migratore.get_db()
         table = db.create_table("users")
         table.add_column("username", type = "text")
         table.add_column("password", type = "text")
-
-    def _get_db(self):
-        try: db = migratore.Migratore.get_db()
-        except: db = None
-        is_skip = not db and hasattr(self, "skipTest")
-        if is_skip: self.skipTest("Not possible to access db")
-        return db
